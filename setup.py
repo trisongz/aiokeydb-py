0c233daf68b2ffc443a2cff7c5e4b544acd98b9a
trisongz <ts@growthengineai.com>
2022-07-07 20:30:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 if sys.version_info.major != 3:
     raise RuntimeError("This package requires Python 3+")
 
-version = '0.0.4'
+version = '0.0.5'
 pkg_name = 'aiokeydb'
 gitrepo = 'trisongz/aiokeydb-py'
 root = Path(__file__).parent
